ba71683e71289c143bf8577f38e9d477debe290c
Manuel Sabogal <manuel.sabogal@king.com>
2024-08-09 14:50:50 +0200

    Added fixed spanish keyboard for K380s

---
+++ app/src/main/java/me/edoren/custom_keyboard_layout/InputDeviceReceiver.java
@@ -0,0 +1,11 @@
+package me.edoren.custom_keyboard_layout;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class InputDeviceReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+    }
+}
